40613943227a436797ed447721511064bcb951df
Javi <maxdevcodes@gmail.com>
2020-12-16 10:50:33 +0100

    refactor generateIcs

--- script.js
+++ script.js
@@ -5,37 +5,31 @@ PRODID:-//hacksw/handcal//NONSGML v1.0//EN
 
 var icsBody = "";
 
-var icsFoot = `END:VCALENDAR`;
+var icsFoot = "END:VCALENDAR";
 
 
 var ics;
 
 function generateIcs(e) {
     e.preventDefault();
-    var form = document.getElementById("form_generator");
-    var childrenForm = form.getElementsByClassName("input");
-    console.log(childrenForm);
     
     icsBody += "BEGIN:VEVENT\n";
 
-    for (const element of childrenForm) {
-        console.log(element.id, element.value);
-        if(element.id == "title") {
-            icsBody += "SUMMARY:" + element.value + "\n";
-        }else if(element.id == "description") {
-            icsBody += "DESCRIPTION:" + element.value + "\n";
-        }else if(element.id == "start_date") {
-            icsBody += "DTSTART:" + element.value.split("-").join("");
-        }else if(element.id == "start_time") {
-            icsBody += "T" + element.value.split(":").join("") + "00\n";
-        }else if(element.id == "end_date") {
-            icsBody += "DTEND:" + element.value.split("-").join("");
-        }else if(element.id == "end_time") {
-            icsBody += "T" + element.value.split(":").join("") + "00\n";
-        }
-    }
+    let title = document.getElementById("title").value;
+    let description = document.getElementById("description").value;
+    let startDate = document.getElementById("start_date").value.split("-").join("");
+    let startTime = document.getElementById("start_time").value.split(":").join("");
+    let endDate = document.getElementById("end_date").value.split("-").join("");
+    let endTime = document.getElementById("end_time").value.split(":").join("");
 
-    icsBody += `END:VEVENT\n`;
+    icsBody += "SUMMARY:" + title + "\n";
+    icsBody += "DESCRIPTION:" + description + "\n";
+    icsBody += "DTSART:" + startDate;
+    icsBody += "T" + startTime + "00\n";
+    icsBody += "DTEND:" + endDate;
+    icsBody += "T" + endTime + "00\n";
+
+    icsBody += "END:VEVENT\n";
 }
 
 function downloadFile() {
